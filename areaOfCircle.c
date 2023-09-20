49902ac6ad021dcb7d2ce0db5936273ef33cf0c3
unknown <sasumro5@gmail.com>
2023-09-20 09:54:07 +0530

    add area of Circle question

---
+++ areaOfCircle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    double radius, area;
+    const double pi = 3.14159265359; 
+
+    printf("Enter the radius of the circle: ");
+    scanf("%lf", &radius);
+
+    area = pi * pow(radius, 2);
+
+    printf("The area of the circle with radius %.2lf is %.2lf\n", radius, area);
+
+    return 0;
+}
